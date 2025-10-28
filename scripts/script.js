ecc20e54898f16fa883b2515d3c83e950c7f2fcb
Md. Affan Hossain Rakib <affanhossainrakib@gmail.com>
2025-10-28 16:45:24 +0600

    feat: implement fortune message display in index.html and script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,6 +1,21 @@
-// placeholder arrow function to test if the script is working
-const testFunction = () => {
-  console.log("Test function is working!");
-};  
+const fortuneMessages = [
+  "You will have a great day!",
+  "Success is on the horizon.",
+  "Happiness is a choice, choose wisely.",
+  "Adventure awaits you.",
+  "You will make a new friend soon.",
+  "A surprise is in store for you.",
+  "Your hard work will soon pay off.",
+  "Believe in yourself and all that you are.",
+  "Opportunities will come your way.",
+  "You will find joy in the little things.",
+];
 
-testFunction();
+const currentMsg = () => {
+  const idx = Math.floor(Math.random() * 9);
+  return fortuneMessages[idx];
+};
+
+
+const fortuneMessgaeParagraph = document.getElementById("fortune-message");
+fortuneMessgaeParagraph.innerText = `${currentMsg()}`;
